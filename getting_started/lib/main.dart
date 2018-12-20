56ee9138a0d2a3c331a6cbd56bbca0413a62212f
Harsh <deadspaceharsh@gmail.com>
2018-12-20 15:36:11 +0530

    First commit

---
+++ getting_started/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(FirstApp());
+
+class FirstApp extends StatelessWidget{
+
+    @override
+      Widget build(BuildContext context) {
+        
+        return new Text("Hello");
+      }
+
+}
